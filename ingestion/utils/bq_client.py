2a969548748b9550ad0c23ee6954221ad51a4a42
SobczakL <36972429+SobczakL@users.noreply.github.com>
2026-06-11 22:27:26 -0400

    chore: testing push to bq

--- ingestion/utils/bq_client.py
+++ ingestion/utils/bq_client.py
@@ -1,6 +1,25 @@
 from google.cloud import bigquery
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 
 def get_client():
     return bigquery.Client(project=os.getenv("GCP_PROJECT_ID"))
+
+
+def insert_rows(dataset: str, table: str, rows: list):
+    client = get_client()
+    table_ref = f"{os.getenv('GCP_PROJECT_ID')}.{dataset}.{table}"
+
+    job = client.load_table_from_json(rows, table_ref)
+    job.result()
+
+    print(f"inserted {len(rows)} rows into {table_ref}")
+
+
+
+def run_query(sql: str):
+    client = get_client()
+    return client.query(sql).to_dataframe()
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import os
+from dotenv import load_dotenv
+from ingestion.utils.bq_client import insert_rows
+
+
+load_dotenv()
+
+
 def main():
+    rows = [
+            {
+                "title": "test headline",
+                "value": "hello world"
+                }
+            ]
+
+    insert_rows("raw", "test", rows)
 
 
 if __name__ == "__main__":
